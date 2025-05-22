e9de84f67253106d4b570eaf809579b7605552ca
johncotter3 <john.cotter.3@gmail.com>
2025-05-21 19:22:33 -0700

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -8,6 +8,13 @@ export default [
     files: ['**/*.js'],
     languageOptions: {
       parser: '@babel/eslint-parser',
+      parserOptions: {
+           ecmaVersion: 'latest',
+           sourceType: 'module',
+           ecmaFeatures: {
+               jsx: false
+           }
+       },
       ecmaVersion: 'latest',
       sourceType: 'module'
     },
